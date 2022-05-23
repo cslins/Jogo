f8190c10340d009c03f420682f25d7f8685f0b62
GabrielSouzadaSilva <g4briel2408@gmail.com>
2022-05-23 16:56:28 -0300

    menu basico
    implementação parcial

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:jogo/main.dart';
@@ -6,7 +8,6 @@ import 'package:jogo/pages/home.dart';
 import 'package:jogo/pages/game.dart';
 
 class HomePage extends StatefulWidget {
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -15,13 +16,37 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white,
-      alignment: Alignment.center,
-      child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/game');
-          },
-          child: Text("Jogar")),
-    );
+        color: Colors.white,
+        // alignment: Alignment.center,
+        child: Row(children: [
+          Container(
+              child: Expanded(
+                  child: Text(
+            'App Ecologico',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'Permanent Marker',
+              fontSize: 55,
+              height: 1,
+              color: Colors.black,
+              inherit: false,
+            ),
+          ))),
+          Container(
+              //margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+              child: Column(mainAxisSize: MainAxisSize.min, children: [
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/game');
+                },
+                child: Text("Jogar")),
+            SizedBox(height: 10),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/settings');
+                },
+                child: Text('Settings'))
+          ]))
+        ]));
   }
 }
---
+++ lib/pages/level_select.dart
@@ -0,0 +1,8 @@
+class name extends StatelessWidget {
+  const name({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
